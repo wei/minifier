51f68d91a23b1f116ca73f86dadf954d119da423
Wei He <git@weispot.com>
2018-05-10 18:42:57 -0400

    Shorter Version String

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ gulp.task('pug', () => {
     __ENV: process.env,
   };
   try {
-    data.__HASH = process.env.COMMIT_REF ||
+    data.__HASH = (process.env.COMMIT_REF || '').substring(0, 7) ||
       childProcess.execSync('git rev-parse --short HEAD').toString().trim() || '';
     data.__BRANCH = process.env.BRANCH ||
       childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim() || '';
